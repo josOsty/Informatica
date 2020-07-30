9dfb45c861af3252153e00b7f62bac6d3d21e749
jose <jao.jarama@yavirac.edu.ec>
2020-07-30 17:27:40 -0500

    "codigo de prubea con tdd con lenguaje oriendata a objetos "

--- Informatica.Negocio.Tests/ComparadorTest.cs
+++ Informatica.Negocio.Tests/ComparadorTest.cs
@@ -23,16 +23,15 @@ namespace Informatica.Negocio.Tests
         [TestMethod]
         public void ObtenerMayor()
         {
-            //Triple A => AAA = Arrange Act Assert
-            //Organizar datos -- Arrange
+           
             var primerElemento = 56;
             var segundoElemento = 20;
             var esperado = 56;
-            //Ejecute -- Act
+          
             Comparador comparador = new Comparador();
             int actual = comparador.ObtenerMayor(primerElemento, segundoElemento);
 
-            //Comprobar --Assert
+          
             Assert.AreEqual(esperado, actual);
         }
     }
